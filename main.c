adc717f80be83b737624106eddbe787f5656943c
Jeffrey Brusseau <bobajeff82@gmail.com>
2023-04-01 13:47:17 -0500

    second pass

--- main.c
+++ main.c
@@ -1,29 +1,25 @@
 #include <GLFW/glfw3.h>
-
 #include <stdio.h>
-// #include <iostream>
 
-int main (int, char**) {
-	if (!glfwInit()) {
-		printf("Could not initialize GLFW!");
-		// std::cerr << "Could not initialize GLFW!" << std::endl;
-		return 1;
-	}
+int main(int argc, char *argv[]) {
+  if (!glfwInit()) {
+    printf("Could not initialize GLFW!");
+    return 1;
+  }
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
-	if (!window) {
-		printf("Could not open window!");
-		// std::cerr << "Could not open window!" << std::endl;
-		glfwTerminate();
-		return 1;
-	}
+  GLFWwindow *window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
+  if (!window) {
+    printf("Could not open window!");
+    glfwTerminate();
+    return 1;
+  }
 
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
-	return 0;
+  return 0;
 }
